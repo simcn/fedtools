cb8e29b2e619099759d08dea998b37510e0d1e56
chenwp <chenwp@Ctrip.com>
2020-06-22 14:48:44 +0800

    up

---
+++ px2vmin/build/precache-manifest.0dfc38d7b319b8fc6967b93c8a5b1c62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7016294ae53a8707dbc228bef457b5e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f1bbdfc255eff9900ac",
+    "url": "/static/css/main.e81e6aa1.chunk.css"
+  },
+  {
+    "revision": "6cd49fe5b5f20971138e",
+    "url": "/static/js/2.8f50f8e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f50f8e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f1bbdfc255eff9900ac",
+    "url": "/static/js/main.0dfcdc73.chunk.js"
+  },
+  {
+    "revision": "685aa7a439caa7cfb387",
+    "url": "/static/js/runtime-main.e5ee5fdc.js"
+  }
+]);
